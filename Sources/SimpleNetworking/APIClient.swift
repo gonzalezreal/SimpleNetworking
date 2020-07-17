5a7b434a7c3160b5ae71bb714c91dc559c3dec86
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-07-17 16:17:48 +0200

    Add documentation for APIClient

--- Sources/SimpleNetworking/APIClient.swift
+++ Sources/SimpleNetworking/APIClient.swift
@@ -26,14 +26,28 @@
     import Foundation
     import Logging
 
+    /// An `APIClient` is an object that makes requests to an API and handles its responses.
+    /// It works in conjunction with the `Endpoint` type, which encapsulates a specific request
+    /// as well as how to decode its response.
     @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
     public class APIClient {
+        /// The base URL to which the endpoint paths are appended.
         public let baseURL: URL
+
+        /// The configuration contains additional headers and query parameters that will be appended to each request.
         public let configuration: APIClientConfiguration
+
+        /// This logger emits requests and responses at debug level.
         public var logger = Logger(label: "APIClient")
 
         private let session: URLSession
 
+        /// Initializes an API client.
+        /// - Parameters:
+        ///   - baseURL: The base URL to which the endpoint paths are appended.
+        ///   - configuration: Additional headers and query parameters that will be appended to each request.
+        ///   - session: The URL session that will handle the requests and responses.
+        ///   - logLevel: The log level. If `.debug`, all requests and responses will be logged.
         public init(
             baseURL: URL,
             configuration: APIClientConfiguration = APIClientConfiguration(),
@@ -46,6 +60,12 @@
             logger.logLevel = logLevel
         }
 
+        /// Returns a publisher that wraps a request for a given endpoint.
+        ///
+        /// The publisher decodes and emits the response when the request completes,
+        /// or terminates with an error if the request fails.
+        ///
+        /// - Parameter endpoint: The endpoint for which to create the request.
         public func response<Output, Error>(for endpoint: Endpoint<Output, Error>) -> AnyPublisher<Output, APIClientError<Error>> {
             let request = URLRequest(baseURL: baseURL, endpoint: endpoint)
                 .addingHeaders(configuration.additionalHeaders)
